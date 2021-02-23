4db73b135efd184bb0819b4bd8e93a705551b11c
JadeJOy <jadehandsome30@gmail.com>
2021-02-23 14:18:29 -0800

    first commit

---
+++ ias-app/build/precache-manifest.c456c335be25cef26304b30edc09ceca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f321973743311f5a3b945f4a37176fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfa2e2528f189189fd20",
+    "url": "/static/css/main.b0831862.chunk.css"
+  },
+  {
+    "revision": "1f0229b18ed28fa6769d",
+    "url": "/static/js/2.9c974d00.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/static/js/2.9c974d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee6cfea04afaf1efa43b",
+    "url": "/static/js/3.0c310c9b.chunk.js"
+  },
+  {
+    "revision": "e2efa734baa9e5c3b8ef165e6121d69e",
+    "url": "/static/js/3.0c310c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fee5deb32b1719532f8",
+    "url": "/static/js/4.09669efe.chunk.js"
+  },
+  {
+    "revision": "94e43c1385bc2f9acbb4",
+    "url": "/static/js/5.3f31a1e8.chunk.js"
+  },
+  {
+    "revision": "a05ee4f50bad3f866742",
+    "url": "/static/js/6.f803f5a6.chunk.js"
+  },
+  {
+    "revision": "bf429a1a9b06497b609d",
+    "url": "/static/js/7.579aafa2.chunk.js"
+  },
+  {
+    "revision": "e17c8fda1f3fd699ef28",
+    "url": "/static/js/8.e41cc9c6.chunk.js"
+  },
+  {
+    "revision": "bfa2e2528f189189fd20",
+    "url": "/static/js/main.2bbd2758.chunk.js"
+  },
+  {
+    "revision": "b763aa40063bc1e1a099",
+    "url": "/static/js/runtime-main.644dbc13.js"
+  },
+  {
+    "revision": "c76eac36caa48d1f50f71e7f1bc9d49f",
+    "url": "/static/media/logo.c76eac36.png"
+  }
+]);
