6e9da571d6280b3f7ed518d3c9a8c27b017398a3
JadeJOy <jadehandsome30@gmail.com>
2021-02-23 18:33:30 -0800

    settings pala with s

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, "ias-app/build")));
 
-app.get("/setting", function (req, res) {
+app.get("/settings", function (req, res) {
   res.sendFile(path.join(__dirname, "ias-app/build", "index.html"));
 });
 
