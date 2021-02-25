4561a19dff66776a679cd79c77482ba91b0ea461
JadeJOy <jadehandsome30@gmail.com>
2021-02-24 17:17:05 -0800

    toString() error

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "ias-app/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "ias-app/build", "index.html"));
   });
 }
