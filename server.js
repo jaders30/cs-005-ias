54a83d5ab22093dad0d579d04d7ca156185449c9
JadeJOy <jadehandsome30@gmail.com>
2021-02-25 23:57:04 -0800

    make it 70% first edit

--- server.js
+++ server.js
@@ -39,8 +39,6 @@ app.use((req, res, next) => {
   // console.log(process.env);
   next();
 });
-
-app.use(express.static(path.join(__dirname, "ias-app/build")));
 app.use(
   helmet({
     contentSecurityPolicy: {
@@ -59,6 +57,7 @@ app.use(
     },
   })
 );
+app.use(express.static(path.join(__dirname, "ias-app/build")));
 
 // ADDED FOR EMAIL CONFIRMATION
 const userEmailSender = "jadewensylofariscal@gmail.com";
